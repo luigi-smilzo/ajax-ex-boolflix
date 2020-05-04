56c37e74ec856c4be76fa7d3c7fb601d9635bc48
Luigi <luigismilzo@live.com>
2020-05-04 16:08:10 +0200

    Milestone 1

--- js/main.js
+++ js/main.js
@@ -4,8 +4,9 @@ $(document).ready(function(){
     var myKey = '?api_key=26864016a30e377316e6a20d4e37109e';
     var searchInput = $('#SearchInput');
     var searchBtn = $('#SearchButton');
+    var movieResults = $('.Results');
 
-    // HANDLEBARS
+    // HANDLEBARS INIT
     var source = $('#list-template').html();
     var template = Handlebars.compile(source);
 
@@ -13,7 +14,11 @@ $(document).ready(function(){
     // Search
     searchBtn.click(function() {
         var search = searchInput.val().trim();
-
+        
+        // reset
+        searchInput.val('');
+        movieResults.html('');
+        
         $.ajax({
             url: myApi + myKey,
             method: 'GET',
@@ -22,21 +27,26 @@ $(document).ready(function(){
                 query: search
             },
             success: function(res) {
-                console.log(res);
+                var results = res.results;
+                
+                for (var i = 0; i < results.length; i++ ) {
+
+                    var context = {
+                        title: results[i].title,
+                        originalTitle: results[i].original_title,
+                        originalLanguage: results[i].original_language,
+                        voteAverage: results[i].vote_average
+                    }
+
+                    var html = template(context);
+                    movieResults.append(html);
+
+                }
             },
             error: function() {
                 console.log('Errore chiamata');
             }
         });
     });
-
-
-
-
-
-
-
-
-
 
 }); //<-- End ready
